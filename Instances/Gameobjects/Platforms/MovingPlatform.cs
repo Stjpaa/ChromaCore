1213806b078286b2a7c4a4601ebb3ad655495b9a
Stjpaa <87935031+Stjpaa@users.noreply.github.com>
2023-03-20 15:56:33 +0100

    Added code for audios
    Added a global audioplayer to the project to play sounds so we can test sounds asap

--- Instances/Gameobjects/Platforms/MovingPlatform.cs
+++ Instances/Gameobjects/Platforms/MovingPlatform.cs
@@ -3,17 +3,13 @@ using System;
 
 public partial class MovingPlatform : Node2D
 {
+	private SoundPlayer _audioPlayer;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		GetNode<AnimationPlayer>("AnimationPlayer").Play("Move");
-	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-		
+		this._audioPlayer = GetNode<SoundPlayer>("/root/SoundPlayer");
 	}
 
 	private void Move(float delta)
--- Instances/Singletons/SoundPlayer.cs
+++ Instances/Singletons/SoundPlayer.cs
@@ -3,16 +3,8 @@ using System;
 
 public partial class SoundPlayer : Node
 {
-	public AudioStreamPlayer _audioPlayer;
-
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
+	public void PlaySound(string Soundname) 
 	{
-		this._audioPlayer = GetNode<AudioStreamPlayer>("AudioPlayers/Audioplayer");
-	}
-
-	public void PlaySound() 
-	{
-		this._audioPlayer.Play();
+		GetNode<AudioStreamPlayer>(Soundname).Play();
 	}
 }
