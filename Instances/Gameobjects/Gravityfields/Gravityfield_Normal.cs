fae751c03057aeba26a7b53b97a530d824155e2f
Stjpaa <87935031+Stjpaa@users.noreply.github.com>
2023-03-20 23:07:52 +0100

    Finished Gravityfields
    - game objects (Player, Box) need the list as a reference so the events can connect
    - removed the ball object (until its needed)

---
+++ Instances/Gameobjects/Gravityfields/Gravityfield_Normal.cs
@@ -0,0 +1,51 @@
+using Godot;
+using System;
+
+[Tool]
+public partial class Gravityfield_Normal : Area2D
+{
+	[Export]
+	private Vector2 _gravityDirection;
+
+	[Export]
+	private float _gravityStrength = 1200;
+
+	[Signal]
+	public delegate void OnGravityfieldEnteredEventHandler(Vector2 direction, float strength);
+
+	[Signal]
+	public delegate void OnGravityfieldExitedEventHandler();
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		this._gravityDirection = GetNode<Node2D>("GravityDirection").Position;
+		
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+			this._gravityDirection = GetNode<Node2D>("GravityDirection").Position;
+			QueueRedraw();
+	}
+
+    public override void _Draw()
+    {
+		if (Engine.IsEditorHint())
+		{
+			DrawLine(new Vector2(0,0), this._gravityDirection, Colors.Blue, 0.3f);
+		}
+        
+    }
+
+	public void OnBodyEntered(Node2D body)
+	{
+		EmitSignal("OnGravityfieldEntered", this._gravityDirection, this._gravityStrength);
+	}	
+
+	public void OnBodyExited(Node2D body)
+	{
+		EmitSignal("OnGravityfieldExited");
+	}
+}
