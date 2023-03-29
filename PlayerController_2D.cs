81c12c7a26616953ff01ac62fee9c79fd8eca0ea
DillonLM00 <79425469+DillonLM00@users.noreply.github.com>
2023-03-29 17:16:57 +0200

    Version 0.0.4
    - Code refactoring for all states
    - Velocity of the playerController is now always added and not set
    - Debug UI added
    - Controls UI added
    - Implemented variable jump height
    - Added falling state

--- PlayerController_2D.cs
+++ PlayerController_2D.cs
@@ -6,8 +6,7 @@ using static PlayerController_2D;
 
 public partial class PlayerController_2D : CharacterBody2D
 {   
-    public bool gravity = true;
-    private const float GRAVITY = 1500;
+
 
     public AnimatedSprite2D AnimatedSprite2D 
     {
@@ -18,22 +17,28 @@ public partial class PlayerController_2D : CharacterBody2D
     public State currentState;
     public State previousState;
 
+    private Label velText;
+    private Label stateText;
+    private Label isOnFloorLabel;
+
     public override void _Ready()
     {
         AnimatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
-        ChangeState(new Idle(this));
+        velText = GetNode<HFlowContainer>("HFlowContainer").GetNode<Label>("Velocity_Label");
+        stateText = GetNode<HFlowContainer>("HFlowContainer").GetNode<Label>("State_Label");
+        isOnFloorLabel = GetNode<HFlowContainer>("HFlowContainer").GetNode<Label>("IsOnFloor_Label");
+        ChangeState(new Falling(this));
     }
 
     public override void _PhysicsProcess(double delta)
     {
-        var velocity = Velocity;
-
-        velocity.Y += (float)delta * GRAVITY;
-        Velocity = velocity;
-
         currentState.Execute(delta);
 
-        MoveAndSlide();                                      
+        stateText.Text = "State: " + currentState.Name;
+        velText.Text = "Velocity: " + Velocity.ToString();
+        isOnFloorLabel.Text = "IsOnFloor: " + IsOnFloor();
+
+        MoveAndSlide();
     }
 
     public void ChangeState(State newState)
@@ -43,8 +48,6 @@ public partial class PlayerController_2D : CharacterBody2D
 
         previousState?.Exit();
         currentState.Enter();
-
-        GD.Print("Changed state to " + currentState.Name);
     }
 
     public abstract class State
@@ -52,7 +55,6 @@ public partial class PlayerController_2D : CharacterBody2D
         public abstract String Name { get; }
 
         protected PlayerController_2D _playerController2D;
-        protected List<Modificator> _modificators;
 
         public State(PlayerController_2D playerController2D)
         {
@@ -74,52 +76,67 @@ public partial class PlayerController_2D : CharacterBody2D
         }
         public override void Execute(double delta)
         {
-
-            var jumpTrigger = Input.IsActionJustPressed("Jump");
-            if(jumpTrigger)
-            {
-                _playerController2D.ChangeState(new VariableJump(_playerController2D));
-            }
-
-            var axis = Input.GetAxis("Move_Left", "Move_Right");
-            if(axis == 0) { return; }
-
-            _playerController2D.ChangeState(new Move(_playerController2D));
-
+            if(CheckJumpingTransition()) { return; }
+            if(CheckMovingTransition()) { return; }
         }
         public override void Exit() { }
+
+        private bool CheckJumpingTransition()
+        {
+            var jumpPressedTrigger = Input.IsActionJustPressed("Jump");
+            if (jumpPressedTrigger)
+            {
+                _playerController2D.ChangeState(new Jumping(_playerController2D));
+                return true;
+            }
+            return false;
+        }
+        private bool CheckMovingTransition()
+        {
+            var axis = Input.GetAxis("Move_Left", "Move_Right");
+            if (axis != 0) 
+            {
+                _playerController2D.ChangeState(new Moving(_playerController2D));
+                return true; 
+            }           
+            return false;
+        }
     }
 
-    public class Move : State
+    public class Moving : State
     {
         public override string Name { get { return "Move"; } }
-        private const float MOVE_SPEED = 250;
 
-        public Move(PlayerController_2D controller) : base(controller) {}
+        public static float MAX_MOVE_SPEED { get{ return 250f; } }
+        public static float MOVE_SPEED_ACCELERATION { get { return 40f; } }
+        public static float MOVE_SPEED_DECCELERATION { get { return 40f; } }
+
+        private bool _movingLeft;
+
+        public Moving(PlayerController_2D controller) : base(controller) { }
         public override void Enter()
         {
-        
+            _movingLeft = Input.IsActionPressed("Move_Left");
         }
         public override void Execute(double delta)
         {
+            if (CheckJumpingTransition()) { return; }
+            if (CheckDashingTransition()) { return; }
+
+            #region Movement
             var moveDirection = Input.GetAxis("Move_Left", "Move_Right");
-            var jumpTrigger = Input.IsActionJustPressed("Jump");
-            var dashTrigger = Input.IsActionJustPressed("Dash");
 
-            var velocity = _playerController2D.Velocity;
-            velocity.X = MOVE_SPEED * moveDirection;
-            _playerController2D.Velocity = velocity;
-
-            if (jumpTrigger)
+            if (moveDirection != 0 && _playerController2D.IsOnFloor())
             {
-                _playerController2D.ChangeState(new Jump(_playerController2D));
+                Acceleration(moveDirection);
             }
-            if (dashTrigger)
+            else
             {
-                _playerController2D.ChangeState(new Dash(_playerController2D));
+                Deccerleration();
             }
+            #endregion
 
-            
+            #region Animation
             // Left
             if (moveDirection < 0)
             {              
@@ -132,145 +149,255 @@ public partial class PlayerController_2D : CharacterBody2D
                 _playerController2D.AnimatedSprite2D.Play("Walk");
                 _playerController2D.AnimatedSprite2D.FlipH = false;
             }
-            else
-            {
-                _playerController2D.ChangeState(new Idle(_playerController2D));
-            }
+
+            #endregion
+
+            if(CheckFallingTransition()) { return; }
+            if(CheckIdleTransition()) { return; }
         }
-        public override void Exit()
-        {
+        public override void Exit() { }
 
+        /// <summary>
+        /// Applies a acceleration on the playerController.
+        /// </summary>
+        private void Acceleration(float direction)
+        {           
+            var velocity = _playerController2D.Velocity;
+            velocity.X += MOVE_SPEED_ACCELERATION * direction;
+            velocity.X = Mathf.Clamp(velocity.X, -MAX_MOVE_SPEED, MAX_MOVE_SPEED);
+            _playerController2D.Velocity = velocity;
         }
-    }
 
-    public class Jump : State
-    {
-        public override string Name { get { return "Jump"; } }
-
-        private const float BASE_JUMP_SPEED = -500;
-
-        public Jump(PlayerController_2D playerController) : base (playerController) { }
-        public override void Enter()
+        /// <summary>
+        /// Applies a deceleration opposite to the playerControllers direction of movement
+        /// </summary>
+        private void Deccerleration()
         {
             var velocity = _playerController2D.Velocity;
-            velocity.Y = BASE_JUMP_SPEED;
-            _playerController2D.Velocity = velocity;
-
-            _playerController2D.ChangeState(_playerController2D.previousState);
-        }
-        public override void Execute(double delta)
-        {
-
-        }
-        public override void Exit()
-        {
-            
-        }
-    }
-
-    public class VariableJump : State
-    {
-        public override string Name { get { return "VariableJump"; } }
-
-        private const float BASE_JUMP_SPEED = -500;
-        private const float MAX_EXTRA_JUMP_SPEED = -250;
-        private float _extraJumpSpeed = -1;
-
-        public VariableJump(PlayerController_2D playerController) : base(playerController) { }
-        public override void Enter()
-        {
-            _playerController2D.AnimatedSprite2D.Play("HoldJump");
-        }
-        public override void Execute(double delta)
-        {
-            var isJumpLoading = Input.IsActionPressed("Jump");
-            var jumpTrigger = Input.IsActionJustReleased("Jump");
-
-            GD.Print(isJumpLoading);
-            
-
-            if(isJumpLoading)
+            if (_movingLeft)
             {
-                _extraJumpSpeed += (float)(MAX_EXTRA_JUMP_SPEED * delta);
-                _extraJumpSpeed = Mathf.Clamp(_extraJumpSpeed, MAX_EXTRA_JUMP_SPEED, 0);
-
-                GD.Print(_extraJumpSpeed);
+                velocity.X += MOVE_SPEED_DECCELERATION;
+                velocity.X = Mathf.Clamp(velocity.X, -MAX_MOVE_SPEED, 0);
             }
+            else
+            {
+                velocity.X -= MOVE_SPEED_DECCELERATION;
+                velocity.X = Mathf.Clamp(velocity.X, 0, MAX_MOVE_SPEED);
+            }
+            _playerController2D.Velocity = velocity;
+        }
 
+        private bool CheckJumpingTransition()
+        {
+            var jumpTrigger = Input.IsActionJustPressed("Jump");
             if (jumpTrigger)
             {
-                var velocity = _playerController2D.Velocity;
-                velocity.Y = BASE_JUMP_SPEED + _extraJumpSpeed;
-                _playerController2D.Velocity = velocity;
-
-                _playerController2D.ChangeState(_playerController2D.previousState);
-            }           
+                _playerController2D.ChangeState(new Jumping(_playerController2D));
+                return true;
+            }
+            return false;
         }
-        public override void Exit()
+        private bool CheckDashingTransition()
+        {           
+            var dashTrigger = Input.IsActionJustPressed("Dash");
+            if (dashTrigger)
+            {
+                _playerController2D.ChangeState(new Dashing(_playerController2D));
+                return true;
+            }
+            return false;
+        }
+        private bool CheckFallingTransition()
         {
-
+            if (_playerController2D.Velocity.X == 0 &&
+                    _playerController2D.IsOnFloor() == false)
+            {
+                _playerController2D.ChangeState(new Falling(_playerController2D));
+                return true;
+            }
+            return false;
+        }
+        private bool CheckIdleTransition()
+        {
+            var moveDirection = Input.GetAxis("Move_Left", "Move_Right");
+            if (_playerController2D.Velocity.X == 0 &&
+                    _playerController2D.IsOnFloor() &&
+                        moveDirection == 0)
+            {
+                _playerController2D.ChangeState(new Idle(_playerController2D));
+                return true;
+            }
+            return false;
         }
     }
 
-    public class Dash : State
+    public class Jumping : State
+    {
+        public override string Name { get { return "Jumping"; } }
+
+        public static float JUMP_SPEED { get { return 250f; } }
+        public static float JUMP_END_MODIFIER { get { return 10f; } }
+
+        public Jumping(PlayerController_2D playerController_2D) : base(playerController_2D) { }
+
+        public override void Enter() { }
+
+        public override void Execute(double delta)
+        {
+            Jump();
+
+            if(CheckFallingTransition()) { return; }
+        }
+        public override void Exit() { }
+
+        private void Jump()
+        {
+            var velocity = _playerController2D.Velocity;
+            velocity.Y -= JUMP_SPEED;
+            _playerController2D.Velocity = velocity;
+        }
+        private bool CheckFallingTransition()
+        {
+            if (_playerController2D.IsOnFloor() == false)
+            {
+                _playerController2D.ChangeState(new Falling(_playerController2D));
+                return true;
+            }
+            return false;
+        }
+    }
+
+    public class Falling : State
+    {
+        public override string Name { get { return "Falling"; } }
+
+        public static float MAX_FALL_SPEED { get { return 700f; } }
+        public static float FALL_SPEED_ACCELERATION { get { return 25f; } }
+        public static float MAX_FALLING_MOVE_SPEED { get {return Moving.MAX_MOVE_SPEED * 0.9f; } }
+        public static float FALLING_MOVE_SPEED_ACCELERATION { get { return Moving.MOVE_SPEED_ACCELERATION * 0.9f; } }
+
+        public Falling(PlayerController_2D playerController_2D) : base(playerController_2D) { }
+
+        public override void Enter()
+        {
+            _playerController2D.AnimatedSprite2D.Play("InAir");
+        }
+        public override void Execute(double delta)
+        {
+            ApplyMovement();
+            ApplyGravity();
+
+            if (CheckTransitionToDash()) { return; }
+            if (CheckTransitionToIdle()) { return; }
+        }
+        public override void Exit()
+        {
+            _playerController2D.Velocity = Vector2.Zero;
+        }
+
+        private void ApplyMovement()
+        {
+            var moveDirection = Input.GetAxis("Move_Left", "Move_Right");
+            var velocity = _playerController2D.Velocity;
+
+            velocity.X += FALLING_MOVE_SPEED_ACCELERATION * moveDirection;
+            velocity.X = Mathf.Clamp(velocity.X, -MAX_FALLING_MOVE_SPEED, MAX_FALLING_MOVE_SPEED);
+
+            _playerController2D.Velocity = velocity;
+        }
+        private void ApplyGravity()
+        {
+            var jumpEndTrigger = Input.IsActionJustReleased("Jump");
+            var velocity = _playerController2D.Velocity;
+
+            velocity.Y += (jumpEndTrigger) ? (FALL_SPEED_ACCELERATION * Jumping.JUMP_END_MODIFIER) : FALL_SPEED_ACCELERATION;
+            velocity.Y = Mathf.Clamp(velocity.Y, float.MinValue, MAX_FALL_SPEED);
+
+            _playerController2D.Velocity = velocity;
+        }
+
+        private bool CheckTransitionToIdle()
+        {
+            if (_playerController2D.IsOnFloor())
+            {
+                _playerController2D.ChangeState(new Idle(_playerController2D));
+                return true;
+            }
+            return false;
+        }
+        private bool CheckTransitionToDash()
+        {
+            var dashTrigger = Input.IsActionJustPressed("Dash");
+            if(dashTrigger)
+            {
+                _playerController2D.ChangeState(new  Dashing(_playerController2D));
+                return true;
+            }
+            return false;
+        }
+    }
+
+    public class Dashing : State
     {
         public override string Name { get { return "Dash"; } }
 
-        private const float DASH_SPEED = 1000;
+        public static float DASH_SPEED { get{ return 1000f; } }
 
-        public Dash(PlayerController_2D playerController) : base (playerController) { }
+        public static float DASH_DURATION { get { return 0.2f; } }
+
+        public Dashing(PlayerController_2D playerController) : base (playerController) { }
         public override void Enter()
-        {
-            var velocity = _playerController2D.Velocity;
-            velocity.X = DASH_SPEED;
-            if(Input.GetAxis("Move_Left", "Move_Right") < 0)
-            {
-                velocity.X *= -1;
-            }
-            _playerController2D.Velocity = velocity;
+        {                      
+            if (CheckFallingOrMovingTransition()) { return; }
 
-            FreezeGravity();
+            Dash();
+          
+            WaitForTimer();
         }
-        public override void Execute(double delta)
-        {
-            _playerController2D.AnimatedSprite2D.Play("Dash");
-        }
+        public override void Execute(double delta) { }
+
         public override void Exit()
         {
-
+            _playerController2D.AnimatedSprite2D.FlipH = false;
         }
-        private async void FreezeGravity()
+        private void Dash()
         {
-            _playerController2D.gravity = false;
-            await _playerController2D.ToSignal(_playerController2D.GetTree().CreateTimer(0.2f), SceneTreeTimer.SignalName.Timeout);
-            _playerController2D.ChangeState(_playerController2D.previousState);
-            _playerController2D.gravity = true;
-        }
-    }
-
-
-    public abstract class Modificator
-    {
-        protected PlayerController_2D _playerController2D;
-
-        public Modificator(PlayerController_2D playerController)
-        {
-            _playerController2D = playerController;
-        }
-        public abstract void Execute(double delta);
-    }
-
-    public class Gravity : Modificator
-    {
-        private const float GRAVITY = 1500;
-
-        public Gravity(PlayerController_2D playerController) : base(playerController) { }
-        public override void Execute(double delta)
-        {
+            var direction = Input.GetAxis("Move_Left", "Move_Right");
             var velocity = _playerController2D.Velocity;
-
-            velocity.Y += (float)delta * GRAVITY;
+            velocity.X = DASH_SPEED * direction;
             _playerController2D.Velocity = velocity;
+
+            #region Animation
+            //Left
+            if (direction < 0)
+            {
+                _playerController2D.AnimatedSprite2D.FlipH = true;
+            }
+            //Right
+            else
+            {
+                _playerController2D.AnimatedSprite2D.FlipH = false;
+            }
+            _playerController2D.AnimatedSprite2D.Play("Dash");
+            #endregion
+        }
+
+        private async void WaitForTimer()
+        {
+            await _playerController2D.ToSignal(_playerController2D.GetTree().CreateTimer(DASH_DURATION), SceneTreeTimer.SignalName.Timeout);
+            _playerController2D.ChangeState(_playerController2D.previousState);
+        }
+
+        private bool CheckFallingOrMovingTransition()
+        {
+            var direction = Input.GetAxis("Move_Left", "Move_Right");
+            // To avoid Dash without direction
+            if (direction == 0)
+            {
+                _playerController2D.ChangeState(_playerController2D.previousState);
+                return true;
+            }
+            return false;
         }
     }
 }
