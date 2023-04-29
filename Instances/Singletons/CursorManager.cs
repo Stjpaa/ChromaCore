b2689c2c4fd877cada0f80270598cd62a57913a3
MarhalmGH <93052627+MarhalmGH@users.noreply.github.com>
2023-04-29 19:18:44 +0200

    Parsing Error fixed
    wenn in Kommentaren  ÄÜÖ, etc verwendet wird entstehen Parsing errors

--- Instances/Singletons/CursorManager.cs
+++ Instances/Singletons/CursorManager.cs
@@ -9,9 +9,9 @@ public partial class CursorManager : Node
     andere Option: Maus verstecken und auf den aktuellen button bewegen, sollte man vielleicht sowieso machen, damit maus auf dem letzten button rauskommt
 
 
-    wenn man maus bewegt soll auf Mausbewegung zurückgeschalten werden, Focus wird entfernt/ ausgeblendet damit nicht 2 button gehighlighted sind.
+    wenn man maus bewegt soll auf Mausbewegung zurueckgeschalten werden, Focus wird entfernt/ ausgeblendet damit nicht 2 button gehighlighted sind.
 
-    man müsste auf den letzten gehoverten button wieder focus setzten und maus ausblenden
+    man muesste auf den letzten gehoverten button wieder focus setzten und maus ausblenden
      */
 
 
