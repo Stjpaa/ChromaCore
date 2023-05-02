7baceb6df54f1b318715ab0f7e638e31c77919ed
FranekStratovarius <louishuss2000@gmail.com>
2023-05-02 10:57:01 +0200

    lasers are finished

--- Instances/Gameobjects/Laser/Laser_Pressure_Plate.cs
+++ Instances/Gameobjects/Laser/Laser_Pressure_Plate.cs
@@ -3,27 +3,25 @@ using Godot.Collections;
 using System;
 
 [Tool]
-public partial class Laser : Node2D
+public partial class Laser_Pressure_Plate : Node2D
 {
-	[Export(PropertyHint.Range, "-3.15,3.15,0.01")]
-	private float _laserAngle;
 	[Export(PropertyHint.Range, "0.0,10,0.1")]
-	private float _enabledTime = 1.0f;
-	[Export(PropertyHint.Range, "0.0,10,0.1")]
-	private float _pauseTime = 2.0f;
-	[Export(PropertyHint.Range, "0.0,10,0.1")]
-	private float _switchTime = 0.1f;
+	private float _switchTime = 0.3f;
 
 	private bool on = true;
 	private float timer;
 
+	private Node2D target;
 	private ColorRect laser_rect;
 	private ShaderMaterial shader_material;
+	private Area2D collider;
 
 	private void RecalculateLaser()
 	{
+		Vector2 target_vector = target.GetGlobalTransform().Origin - this.GetGlobalTransform().Origin;
+		float laser_angle = target_vector.Angle();
 		PhysicsDirectSpaceState2D spaceState = GetWorld2D().DirectSpaceState;
-		PhysicsRayQueryParameters2D query = PhysicsRayQueryParameters2D.Create(this.GetGlobalTransform().Origin, this.GetGlobalTransform().Origin + Vector2.FromAngle(_laserAngle) * 1000);
+		PhysicsRayQueryParameters2D query = PhysicsRayQueryParameters2D.Create(this.GetGlobalTransform().Origin, this.GetGlobalTransform().Origin + Vector2.FromAngle(laser_angle) * 1000);
 		query.CollideWithAreas = true;
 		Dictionary result = spaceState.IntersectRay(query);
 
@@ -31,7 +29,7 @@ public partial class Laser : Node2D
 		
 		if (result.Count > 0)
 		{
-			if (!Engine.IsEditorHint())
+			if (!Engine.IsEditorHint() && on)
 			{
 				try
 				{
@@ -52,48 +50,40 @@ public partial class Laser : Node2D
 		}
 
 		laser_rect.Size = new Vector2(path_length, 30);
-		laser_rect.Rotation = _laserAngle;
+		laser_rect.Rotation = laser_angle;
 		laser_rect.Position = new Vector2(0, -15);
 	}
 
 	public override void _Ready()
 	{
+		target = GetNode<Node2D>("Laser_Target");
+		collider = GetNode<Node2D>("Pressure_Plate").GetNode<Area2D>("Pressure_Plate_Collision");
 		laser_rect = GetNode<ColorRect>("Laser_Rect");
 		shader_material = laser_rect.Material as ShaderMaterial;
 		RecalculateLaser();
 
-		timer = _enabledTime;
+		collider.BodyEntered += Toggle;
 	}
 
 	public override void _Process(double delta)
 	{
 		RecalculateLaser();
-		timer -= (float)delta;
-		if (timer <= 0)
+		if (timer >= 0)
 		{
+			timer -= (float)delta;
+			float progress = Math.Clamp(timer, 0.0f, _switchTime) / _switchTime;
 			if (on)
 			{
-				on = false;
-				timer = _pauseTime;
-			}
-			else
-			{
-				on = true;
-				timer = _enabledTime;
-			}
-		}
-		else
-		{
-			float progress = 0.0f;
-			if (on)
-			{
-				progress = _enabledTime - timer;
-			}
-			else
-			{
-				progress = _switchTime - (_pauseTime - timer);
+				progress = 1 - progress;
 			}
 			shader_material.SetShaderParameter("progress", Math.Clamp(progress, 0.0f, _switchTime) / _switchTime);
 		}
+	}
+
+	private void Toggle(Node2D body)
+	{
+		GD.Print($"body entered: {body}");
+		timer = _switchTime;
+		on = !on;
 	}
 }
