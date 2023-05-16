faaa41ac63e20bc4643400fc3db3ca633e83f9bc
MarhalmGH <93052627+MarhalmGH@users.noreply.github.com>
2023-05-16 13:09:08 +0200

    Loading Screen Path

--- Instances/Menus/LoadingScreen.cs
+++ Instances/Menus/LoadingScreen.cs
@@ -6,79 +6,105 @@ using System.Threading.Tasks;
 public partial class LoadingScreen : Control
 {
     private AnimationPlayer LoadingscreenAnimationPlayer;
+    [Export] private Control loadingScreenBackground;
 
     [Export] private PackedScene testLevelToLoad;
 
     public override void _Ready()
     {
-        LoadingscreenAnimationPlayer = (AnimationPlayer)GetNode("LoadingScreenAnimation");
+        LoadingscreenAnimationPlayer = (AnimationPlayer)GetNode("LoadingScreenAP");
         StartLoadSceneAsync(testLevelToLoad.ResourcePath);
     }
 
     private void StartLoadSceneAsync(string sceneToLoadPath)
     {
-        _ = LoadSceneAsync(sceneToLoadPath);    // _ = means the value gets discarded on completion 
+        //_ = LoadSceneAsync(sceneToLoadPath);    // _ = means the value gets discarded on completion 
+        _ = LoadingScreenAsync(new Image(), new Image());    // _ = means the value gets discarded on completion 
     }
 
-    private async Task LoadSceneAsync(string sceneToLoad)
+    //private async Task LoadSceneAsync(string sceneToLoad)
+    //{
+    //    ResourceLoader.LoadThreadedRequest(sceneToLoad);    // initiate the Background Loading
+
+    //    var sceneLoadStatus = ResourceLoader.LoadThreadedGetStatus(sceneToLoad);
+
+
+
+
+
+    //    do    // ensures the middle animation always gets played at least once
+    //    {
+    //        await LoadingScreenLoopAnimation();
+    //    } while (sceneLoadStatus == ResourceLoader.ThreadLoadStatus.InProgress);
+
+
+    //    // maybe switch scene and pause game before fading out the loading screen
+    //    await LoadingScreenEndAnimation();
+
+    //    var loadedScene = (PackedScene)ResourceLoader.LoadThreadedGet(sceneToLoad);     // Change to the Loaded Scene
+    //    GetTree().ChangeSceneToPacked(loadedScene);
+
+
+    //}
+
+
+    public async Task LoadingScreenAsync(Image startLocation, Image destination)
     {
-        ResourceLoader.LoadThreadedRequest(sceneToLoad);    // initiate the Background Loading
+        ShowLoadingScreen();
 
-        var sceneLoadStatus = ResourceLoader.LoadThreadedGetStatus(sceneToLoad);
-
-
-
-        await LoadingScreenStartAnimation();
-
-
-
-
-        do    // ensures the middle animation always gets played at least once
-        {
-            await LoadingScreenLoopAnimation();
-        } while (sceneLoadStatus == ResourceLoader.ThreadLoadStatus.InProgress);
-
-
-        // maybe switch scene and pause game before fading out the loading screen
-        await LoadingScreenEndAnimation();
-
-        var loadedScene = (PackedScene)ResourceLoader.LoadThreadedGet(sceneToLoad);     // Change to the Loaded Scene
-        GetTree().ChangeSceneToPacked(loadedScene);
-
-
-    }
-
-
-    private async Task LoadingScreenStartAnimation()
-    {
-        LoadingscreenAnimationPlayer.Play("LoadingScreenStartAnimation");
+        LoadingscreenAnimationPlayer.Play("RocketFlight");
 
         while (LoadingscreenAnimationPlayer.IsPlaying())    // wait until Animation is done
         {
             // Wait for a short time before checking again
             await Task.Delay(50);
         }
+
+
+        HideLoadingScreen();
+
+        // done
     }
 
-    async Task LoadingScreenEndAnimation()
+    private void HideLoadingScreen()
     {
-        LoadingscreenAnimationPlayer.Play("LoadingScreenEndAnimation");
-
-        while (LoadingscreenAnimationPlayer.IsPlaying())    // wait until Animation is done
-        {
-            // Wait for a short time before checking again
-            await Task.Delay(50);
-        }
     }
 
-    async Task LoadingScreenLoopAnimation()
+    private void ShowLoadingScreen()
     {
-        LoadingscreenAnimationPlayer.Play("LoadingScreenLoopAnimation");
-
-        while (LoadingscreenAnimationPlayer.IsPlaying())    // wait until Animation is done
-        {
-            // Wait for a short time before checking again
-            await Task.Delay(50);
-        }
     }
+
+
+    //private async Task LoadingScreenStartAnimation()
+    //{
+    //    LoadingscreenAnimationPlayer.Play("LoadingScreenStartAnimation");
+
+    //    while (LoadingscreenAnimationPlayer.IsPlaying())    // wait until Animation is done
+    //    {
+    //        // Wait for a short time before checking again
+    //        await Task.Delay(50);
+    //    }
+    //}
+
+    //async Task LoadingScreenEndAnimation()
+    //{
+    //    LoadingscreenAnimationPlayer.Play("LoadingScreenEndAnimation");
+
+    //    while (LoadingscreenAnimationPlayer.IsPlaying())    // wait until Animation is done
+    //    {
+    //        // Wait for a short time before checking again
+    //        await Task.Delay(50);
+    //    }
+    //}
+
+    //async Task LoadingScreenLoopAnimation()
+    //{
+    //    LoadingscreenAnimationPlayer.Play("LoadingScreenLoopAnimation");
+
+    //    while (LoadingscreenAnimationPlayer.IsPlaying())    // wait until Animation is done
+    //    {
+    //        // Wait for a short time before checking again
+    //        await Task.Delay(50);
+    //    }
+    //}
 }
