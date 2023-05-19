4c0b72c7a54b2d69ba7d27eaa1a83e218bdf92e7
Stjpaa <87935031+Stjpaa@users.noreply.github.com>
2023-05-19 13:34:40 +0200

    Re-organized sprites and added flag sprite to checkpoint scene

--- Instances/Gameobjects/Traps and Checkpoints/Checkpoint.cs
+++ Instances/Gameobjects/Traps and Checkpoints/Checkpoint.cs
@@ -1,9 +1,28 @@
 using Godot;
 using System;
 
+[Tool]
 public partial class Checkpoint : Area2D
 {
-	public delegate void OnCheckPointReachedEventHandler(Vector2 location);
+	[Export]
+	private float _collisionWidth = 50f;
+
+	private CollisionShape2D _collider;
+
+	public override void _Ready() 
+	{
+		this._collider = GetNode<CollisionShape2D>("Collider");
+		this._collider.Shape.Set("size", new Vector2(this._collisionWidth, 100));
+	}
+
+	public override void _Process(double delta)
+	{
+		if (Engine.IsEditorHint())
+		{
+			this._collider.Shape.Set("size", new Vector2(this._collisionWidth, 100));
+		}
+	}
+
 
 	public void OnBodyEntered(Node2D body)
 	{
