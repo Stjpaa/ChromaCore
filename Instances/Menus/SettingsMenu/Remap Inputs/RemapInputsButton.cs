647d2c993101a7794534863ca90d60a70bfcf018
MarhalmGH <93052627+MarhalmGH@users.noreply.github.com>
2023-06-05 00:34:33 +0200

    enum + switch erstellt

---
+++ Instances/Menus/SettingsMenu/Remap Inputs/RemapInputsButton.cs
@@ -0,0 +1,15 @@
+using Godot;
+using System;
+
+public partial class RemapInputsButton : Button
+{
+    [Export] public RemapInputs.RemapedInputs inputToRemapType;
+    [Export] public int positionInRemapArray = 0;
+    [Export] private RemapInputs remapInputs;
+    [Export] public string actionName = "ui_up";
+
+    public override void _Pressed()
+    {
+        remapInputs.OnButtonPressed(this);
+    }
+}
