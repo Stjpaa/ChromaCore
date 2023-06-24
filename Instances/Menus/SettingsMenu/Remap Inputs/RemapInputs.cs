fb6e8d9488fd83c4297aafd5a851cf988c905f72
MarhalmGH <93052627+MarhalmGH@users.noreply.github.com>
2023-06-24 06:01:11 +0200

    Code Cleanup

--- Instances/Menus/SettingsMenu/Remap Inputs/RemapInputs.cs
+++ Instances/Menus/SettingsMenu/Remap Inputs/RemapInputs.cs
@@ -19,13 +19,6 @@ public partial class RemapInputs : Control
 
     private InputsResource inputsResource;
 
-
-    private InputEvent keyboardOne;
-    private InputEvent keyboardTwo;
-    private InputEvent controllerOne;
-
-    private string actionName = "ui_up";
-
     public override void _Ready()
     {
         inputPanel = (Panel)GetNode("SetInputPanel");
@@ -34,49 +27,8 @@ public partial class RemapInputs : Control
         {
             inputsResource = InputsResource.LoadInputsResource();
         }
-
-
-
-        //GD.PrintErr("could cause Problems ith the order, because new actions would cause controllerOne to move up to [1] in the InputList should probalby safe new Button");
-
-        //UpdateCurrentInputevents(actionName);
-
-        //PrintAllInputs(actionName);
-
-        //InputEvent replacementEvent = controllerOne;
-        //ReplaceInputEvent(actionName, keyboardOne, replacementEvent);
-        //PrintAllInputs(actionName);
     }
 
-    //private void UpdateCurrentInputevents(string action)
-    //{
-    //    var currentInputEvents = InputMap.ActionGetEvents(action);
-
-    //    keyboardOne = currentInputEvents[0];
-    //    keyboardTwo = currentInputEvents[1];
-    //    controllerOne = currentInputEvents[2];
-    //}
-
-    private void PrintAllInputs(string action)
-    {
-        foreach (var input in InputMap.ActionGetEvents(action))
-        {
-            GD.Print(input.AsText());
-        }
-    }
-
-    /// <summary>
-    /// Removes a Event that is currently in the InputMap and Replaces it with a new one.
-    /// 
-    /// </summary>
-    public void ReplaceInputEvent(string action, InputEvent eventToRemove, InputEvent eventToAdd)
-    {
-        //causes Problems if the same Input is assigned twice to the same action,
-        //or if it is part of a different action (for example up and rigth are both assigned d -> causes Problem)
-
-        InputMap.ActionEraseEvent(action, eventToRemove);
-        InputMap.ActionAddEvent(action, eventToAdd);
-    }
 
     public override void _Input(InputEvent inputEvent)
     {
