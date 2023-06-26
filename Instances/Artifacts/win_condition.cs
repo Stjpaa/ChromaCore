12a46882ea16db43acdaf0efa9a5e4966f623c43
MarhalmGH <93052627+MarhalmGH@users.noreply.github.com>
2023-06-26 08:02:54 +0200

    Wincondition works

--- Instances/Artifacts/win_condition.cs
+++ Instances/Artifacts/win_condition.cs
@@ -34,8 +34,28 @@ public partial class win_condition : Node2D
 
     private void WinGame()
     {
-        GD.Print("Game Won");
+        var winScreen = (CanvasLayer)GetNode("CanvasLayer");
+        winScreen.Visible = true;
+
+        GetTree().Paused = true;    // affects all Nodes that have the property: Node -> Mode -> Inherit .    this Node should be set to Always instead
+
+        Input.MouseMode = Input.MouseModeEnum.Hidden;
     }
 
+    public void QuitGame()
+    {
+        LevelInstantiater instantiaterOfScene = (LevelInstantiater)GetTree().Root.GetNode("LevelInstantiater");
 
+        if (instantiaterOfScene == null)
+        {
+            GD.PrintErr("No LevelInstanciater in Scene, Exit game instead.");
+            GetTree().Quit();
+            return;
+        }
+        else
+        {
+            _ = instantiaterOfScene.QuitLevelAsync();
+        }
+
+    }
 }
