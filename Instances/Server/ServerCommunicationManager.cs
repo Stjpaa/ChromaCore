5604a8d0ed0f9e7ac596bf463c04d0b2b59496ad
MarhalmGH <93052627+MarhalmGH@users.noreply.github.com>
2023-06-29 11:55:38 +0200

    conversion dict to json

--- Instances/Server/ServerCommunicationManager.cs
+++ Instances/Server/ServerCommunicationManager.cs
@@ -11,8 +11,7 @@ public partial class ServerCommunicationManager : Node2D
 
     public override void _Ready()
     {
-        Test();
-        Test();
+        SendDeath(testTime, testX, testY);
     }
 
     public void SendRQ(float time, float posX, float posY)
@@ -27,58 +26,31 @@ public partial class ServerCommunicationManager : Node2D
 
     private void SendDeathOrRQ(string url,float time, float posX, float posY)
     {
-        //level = level;
-        //Json json = new Json().ToString{ 
-        //{
-        //    "time":921769, // milisekunden
-        //    "level":1,
-        //    "positionx":20,
-        //    "positiony":40
-        //}
-        //}
+
+        var dataAsDict = new Godot.Collections.Dictionary
+        {
+            { "time", time },
+            { "level", level },
+            { "positionx", posX },
+            { "positiony", posY }
+        };
+
+        string json = Json.Stringify(dataAsDict);
+
+
+        var httpRequest = new HttpRequest();
+        AddChild(httpRequest);
+
+        Error error = httpRequest.Request(url);
+        if (error != Error.Ok)
+        {
+            GD.PushError("An error occurred in the HTTP request.");
+        }
 
     }
 
     public void SendWin(float time)
     {
 
-    }
-
-
-    private void Test()
-    {
-        var httpRequest = new HttpRequest();
-        AddChild(httpRequest);
-        httpRequest.RequestCompleted += HttpRequestCompleted;
-
-        // Perform a GET request. The URL below returns JSON as of writing.
-        Error error = httpRequest.Request("https://httpbin.org/get");
-        if (error != Error.Ok)
-        {
-            GD.PushError("An error occurred in the HTTP request.");
-        }
-
-        //// Perform a POST request. The URL below returns JSON as of writing.
-        //// Note: Don't make simultaneous requests using a single HTTPRequest node.
-        //// The snippet below is provided for reference only.
-        //Json msg = new Json();
-        //string body = msg.Stringify(new Godot.Collections.Dictionary{{ "name", "Godette" }});
-        //error = httpRequest.Request("https://httpbin.org/post", null, true, HttpClient.Method.Post, body);
-        //if (error != Error.Ok)
-        //{
-        //    GD.PushError("An error occurred in the HTTP request.");
-        //}
-    }
-
-    // Called when the HTTP request is completed.
-    private void HttpRequestCompleted(long result, long responseCode, string[] headers, byte[] body)
-    {
-        var json = new Json();
-        json.Parse(body.GetStringFromUtf8());
-        //var response = json.GetData().AsGodotDictionary();
-        var response = json.Data.AsGodotDictionary();
-
-        //// Will print the user agent string used by the HTTPRequest node (as recognized by httpbin.org).
-        GD.Print("request completed: " + (response["headers"].AsGodotDictionary())["User-Agent"]);
     }
 }
